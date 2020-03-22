4a6a013c11f1e84282e0783a65f0b981d9805e94
t1 <swjeong@jbcp.kr>
2020-03-23 08:36:37 +0900

    20200323_3

--- src/api.js
+++ src/api.js
@@ -1,64 +1,10 @@
 const express = require("express");
-const cookieParser = require('cookie-parser');
-
-
-const path = require('path');
-const session = require('express-session');
-const flash = require('connect-flash');
-require('dotenv').config();
-
-const pageRouter = require('./routes/page');
-
-const app = express();
-const router = express.Router();
-
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.use(cookieParser(process.env.COOKIE_SECRET));
-
-app.use(cookieParser('nodebirdsecret'));
-app.use(session({
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET,
-    cookie: {
-        httpOnly: true,
-        secure: false,
-    },
-}));
-app.use(flash());
-
-router.get('/', pageRouter);
-
-router.get((req, res, next)=>{
-    const err= new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-router.get((err, req, res)=>{
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : [];
-    res.status(err.status || 500);
-    res.render('error');
-})
-
 
 const serverless = require('serverless-http');
 
+const app = express();
 
-// router.get('/', (req, res)=>{
-//     res.json({
-//         'hello': 'hi!'
-
-//     });
-// });
-
+const router = express.Router();
 
 router.get('/test', (req, res)=>{
     res.json({

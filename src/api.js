9671a92ffdd7f05eb5c77ca155f1dd7228822554
t1 <swjeong@jbcp.kr>
2020-03-23 15:57:59 +0900

    202003231557

--- src/api.js
+++ src/api.js
@@ -42,7 +42,14 @@ router.get('/test', (req, res)=>{
     });
 });
 router.get('/profile', (req, res)=>{
-    res.render('profile', { title: '내 정보 - NodeBird', user: null});
+    try{
+        res.render('profile', { title: '내 정보 - NodeBird', user: null});
+    }catch(err{
+        res.json({
+            'err': err
+        });
+    }
+    
 });
 router.get('/test2/:n1/:n2', (req, res)=>{
     res.json({

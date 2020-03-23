9ef6cf314692e69164df7532d6ce40a9367bd5f9
t1 <swjeong@jbcp.kr>
2020-03-23 09:06:05 +0900

    20200323_4

--- src/api.js
+++ src/api.js
@@ -1,10 +1,39 @@
 const express = require("express");
+const cookieParser = require('cokie-parser');
+
+const path  = require('path');
+const session = require('express-session');
+const flash = require('connect-flash');
+
+const pageRouter = require('./routes/page');
+
+
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine', 'pug');
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({exteded: false}));
+app.use(cookiesParser('nodebirdsecret'));
+app.use(seesion({
+    resave: false,
+    saveUninitialized: false,
+    secret: 'nodebirdsecret',
+    cookie:{
+        httpOnly: true,
+        secure: false,
+    },
+}));
+app.use(flash());
+//app.use('/', pageRouter);
 
 const serverless = require('serverless-http');
 
 const app = express();
 
 const router = express.Router();
+router.get('/', pageRouter);
 
 router.get('/test', (req, res)=>{
     res.json({

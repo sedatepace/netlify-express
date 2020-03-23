08dae970f4e3ace42c2b42320cc0696a075106b5
t1 <swjeong@jbcp.kr>
2020-03-23 09:08:34 +0900

    20200323

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const cookieParser = require('cokie-parser');
+const cookieParser = require('cookie-parser');
 
 const path  = require('path');
 const session = require('express-session');
